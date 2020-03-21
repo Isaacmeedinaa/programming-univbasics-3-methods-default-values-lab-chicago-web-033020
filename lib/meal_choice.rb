abbe233764f416fd1175c841fe4cf0acc780b53f
Isaac Medina <isaac.meedinaa@gmail.com>
2020-03-21 18:43:28 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -10,3 +10,10 @@ end
 
 meal_choice("broccoli", "macaroni")
 meal_choice("broccoli", "macaroni", "roast beef")
+
+def meal_choice(veg1, veg2, protein = 'tofu')
+  puts "What a nutritious meal!"
+  meal = "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts meal
+  meal
+end
